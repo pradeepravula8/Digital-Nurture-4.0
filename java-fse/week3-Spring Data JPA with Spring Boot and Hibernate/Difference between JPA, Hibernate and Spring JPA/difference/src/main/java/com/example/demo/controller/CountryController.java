b9276528861d750a2a5d1cf489c019ce02c6a054
pradeepravula8 <ravulapradeep8@gmail.com>
2025-07-07 00:22:02 +0530

    Pushed spring maven  project to existing folder

---
+++ java-fse/week3-Spring Data JPA with Spring Boot and Hibernate/Difference between JPA, Hibernate and Spring JPA/difference/src/main/java/com/example/demo/controller/CountryController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Country;
+import com.example.demo.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
